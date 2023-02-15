43d6bc74701b2790d5ca1e8c670fd02bf8c7b0f1
anandkrishna15 <124715274+anandkrishna15@users.noreply.github.com>
2023-02-15 12:00:42 +0530

    expt2/.

---
+++ values.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstdlib>
+#include <cstdio>
+using namespace std;
+
+int main()
+{
+    for (int i = 1; i <= 100000; i++)
+    {
+        cout << rand() << " ";
+    }
+
+    return 0;
+}
+
